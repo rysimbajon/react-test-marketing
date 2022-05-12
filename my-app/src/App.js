cd55294dabfe330f509761ba6372552234997de2
rysimbajon <rysimbajon@gmail.com>
2022-05-12 16:18:33 +0800

    updated labels only

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -72,8 +72,8 @@ function ProductView() {
                     <Box sx={{ width: '100%' }}>
                         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                            <Tab label="Item One" {...a11yProps(0)} />
-                            <Tab label="Item Two" {...a11yProps(1)} />
+                            <Tab label="Description" {...a11yProps(0)} />
+                            <Tab label="Details" {...a11yProps(1)} />
                             </Tabs>
                         </Box>
                         <TabPanel value={value} index={0}>
